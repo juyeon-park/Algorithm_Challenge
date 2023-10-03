22e81c9513b1e487b6332d618a8a7c276fc1a47a
judahhh <jdh99080516@gmail.com>
2023-10-03 23:24:52 +0900

    feat : [PGS] 혼자 놀기의 달인 / lv2

--- 1주차 (10.03 - 10.08)/주다현_혼자놀기의달인.js
+++ 1주차 (10.03 - 10.08)/주다현_혼자놀기의달인.js
@@ -1,7 +1,23 @@
 function solution(cards) {
-    let answer = 0
-
-
-
-    return answer
-}
+  
+    const answer = []
+     cards.forEach((v, i) => {
+       let idx = i
+       let count = 0
+       while (true) {
+         if (cards[idx]) {
+           const temp = cards[idx]
+           cards[idx] = 0
+           idx = temp - 1
+           count++
+         } else {
+           answer.push(count)
+           break
+         }
+       }
+     })
+     const sortAnswer = answer.filter((v) => v != 0).sort((a, b) => b - a)
+   
+     return sortAnswer.length > 1 ? sortAnswer[0] * sortAnswer[1] : 0
+   
+   }
